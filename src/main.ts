1b0322dd8db8dd881cc560105bed33fe59554413
Ostap Prodanyuk <OstapoKapo@gmail.com>
2025-08-30 04:15:14 +0300

    feat: add api settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+
+
+dotenv.config({path: '.env.local'})
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token'],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('to-do app API')
+    .setDescription('API for managing to-do items')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const PORT = process.env.PORT;
+  if(!PORT) throw new Error("No PORT specified in env.local file");
+
+  await app.listen(PORT);
 }
 bootstrap();
